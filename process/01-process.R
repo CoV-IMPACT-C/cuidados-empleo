5c8bbf56cff2186e46f6bcfd303c61a089e2538d
Valentina Andrade <valentina.vasquez.a@ug.uchile.cl>
2020-11-30 13:29:20 -0300

    empleo cuidados
    process ene - analisis ocupacion pandemia y cuidados

---
+++ process/01-process.R
@@ -0,0 +1,10 @@
+# Code 1: Process ENE -----------------------------------------------------
+
+# 1. Cargar librerias -----------------------------------------------------
+
+
+# 2. Cargar base de datos -------------------------------------------------
+
+ene <- haven::read_dta("https://www.ine.cl/docs/default-source/ocupacion-y-desocupacion/bbdd/2020/stata/ene-2020-01-def.dta?sfvrsn=3dabbf2d_11&amp;download=true")
+
+#"https://www.ine.cl/docs/default-source/ocupacion-y-desocupacion/bbdd/2020/stata/ene-2020-01-def.dta?sfvrsn=3dabbf2d_11&amp;download=true"
